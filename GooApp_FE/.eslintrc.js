658098febbb44be5b709e660a417e10c62860112
itismilob <itismilob@gmail.com>
2025-05-10 22:11:15 +0900

    init

---
+++ GooApp_FE/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: '@react-native',
+  // babel config setting
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+};
---
+++ GooApp_FE/src/App.tsx
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+function App(): React.JSX.Element {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{color: 'white'}}>This is Test!</Text>
+    </View>
+  );
+}
+
+export default App;
