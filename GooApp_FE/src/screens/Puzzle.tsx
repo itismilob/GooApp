0d5b6f2efea7f073ab426fe75d0c68950d900338
itismilob <itismilob@gmail.com>
2025-05-16 19:03:25 +0900

    feat: 퍼즐 화면 생성
    점수판으로 이동

--- GooApp_FE/src/screens/Puzzle.tsx
+++ GooApp_FE/src/screens/Puzzle.tsx
@@ -1,5 +1,25 @@
-import { Text } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { DefaultNavigatorParams } from '../types/navigationTypes';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Puzzle() {
-  return <Text>Puzzle</Text>;
+  type NavigationProp = NativeStackNavigationProp<
+    DefaultNavigatorParams,
+    'Puzzle'
+  >;
+  const navigation = useNavigation<NavigationProp>();
+
+  return (
+    <View>
+      <Text>Puzzle</Text>
+      <Pressable
+        onPress={() => {
+          navigation.navigate('Scoreboard');
+        }}
+      >
+        <Text>점수판 이동</Text>
+      </Pressable>
+    </View>
+  );
 }
