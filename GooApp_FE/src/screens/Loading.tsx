456cd6bb4ceb2ed43993f7d65d53a91ecaf49b2d
itismilob <itismilob@gmail.com>
2025-05-23 21:53:23 +0900

    feat: MMKV를 사용해서 첫 실행 확인 기능 제작
    stores/mmkvStorage 생성
    Loading.tsx 첫 실행 확인 기능 생성

--- GooApp_FE/src/screens/Loading.tsx
+++ GooApp_FE/src/screens/Loading.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { DefaultNavigatorParams } from '../types/navigationTypes';
@@ -7,6 +7,7 @@ import { useNavigation } from '@react-navigation/native';
 import StyledText from '../components/StyledText';
 
 import { useNetInfo } from '@react-native-community/netinfo';
+import { getLocalStorage } from '../stores/mmkvStorage';
 
 export default function Loading() {
   type NavigationProp = NativeStackNavigationProp<
@@ -14,20 +15,39 @@ export default function Loading() {
     'Loading'
   >;
   const navigation = useNavigation<NavigationProp>();
-
-  // 첫 실행 확인 더미 데이터
-  const isFirst = true;
+  const LocalStorage = getLocalStorage();
   const NetInfo = useNetInfo();
+  const [isFirst, setIsFirst] = useState<boolean>();
+
+  // 첫 실행인지 확인
+  function checkFirstStart() {
+    // userDataString : 유저 데이터가 존재하는지 확인 (임시 명칭임)
+    const userDataString = LocalStorage.getString('userData');
+    return userDataString === undefined;
+  }
+
+  // 네트워크 확인해서 모달 띄움
+  function checkNetwork() {
+    if (NetInfo.isConnected === true) {
+      navigation.replace('NicknameNoti');
+    } else if (NetInfo.isConnected === false) {
+      navigation.navigate('NetworkOfflineModal');
+    }
+  }
 
   useEffect(() => {
-    if (isFirst) {
-      if (NetInfo.isConnected === true) {
-        navigation.replace('NicknameNoti');
-      } else if (NetInfo.isConnected === false) {
-        navigation.navigate('NetworkOfflineModal');
-      }
-    } else {
+    // 첫 실행인지 확인하고 아니면 Home으로 이동
+    const result = checkFirstStart();
+    setIsFirst(result);
+    if (!result) {
       navigation.replace('Home');
+    }
+  }, []);
+
+  useEffect(() => {
+    // 첫 실행인지 확인이 된 후 네트워크 확인
+    if (isFirst) {
+      checkNetwork();
     }
   }, [NetInfo]);
 
