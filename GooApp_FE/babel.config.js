8f40d011eaf84da3defe666aad191d974dee93a3
itismilob <itismilob@gmail.com>
2025-05-27 16:17:13 +0900

    build: babel-plugin-module-resolver 설치
    babel.config 수정 - path alias 설정
    tsconfig 수정 - path alias 설정

--- GooApp_FE/babel.config.js
+++ GooApp_FE/babel.config.js
@@ -1,4 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
-  plugins: ['module:react-native-dotenv'],
+  plugins: [
+    'module:react-native-dotenv',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
