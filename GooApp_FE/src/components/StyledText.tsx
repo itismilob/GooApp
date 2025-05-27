01a7834f9bbe9bc9a9da5a55d4f9a9295845a554
itismilob <itismilob@gmail.com>
2025-05-27 21:45:22 +0900

    fix: 디자인 수정 및 글자 기본 색상 수정

--- GooApp_FE/src/components/StyledText.tsx
+++ GooApp_FE/src/components/StyledText.tsx
@@ -7,8 +7,13 @@ interface props extends TextProps {
 export default function StyledText({ className, ...rest }: props) {
   const font = {
     fontFamily: 'Pretendard-Bold',
-    color: 'white',
+    color: '',
   };
+
+  // 글자 색이 설정되지 않으면 기본값 흰색
+  if (!className?.includes(' color-')) {
+    font.color = 'white';
+  }
 
   return <Text style={font} className={className} {...rest} />;
 }
