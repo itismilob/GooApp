36ee4448dbd6fe672afbe8026539a6b51b880123
itismilob <itismilob@gmail.com>
2025-06-06 23:05:57 +0900

    feat: 랭크 페이지 제작
    Rank.tsx: 랭크 페이지
    - postUserData : 유저 정보를 전송하고 변동된 랭크를 받아옴, 변경사항 로컬에 저장
    - getRankList : 랭크 리스트를 받아옴
    
    RankListLine : 랭크 페이지를 위한 리스트 컴포넌트 (RecordListLine과 동일함)
    
    dataTypes : 유저 데이터에 topScore 추가
    
    rankData.json : 랭크 리스트 더미 데이터

--- GooApp_FE/src/screens/Rank.tsx
+++ GooApp_FE/src/screens/Rank.tsx
@@ -1,5 +1,111 @@
-import { Text } from 'react-native';
+import HeaderButton from '@/components/HeaderButton';
+import { View, ScrollView } from 'react-native';
+import TitleText from '@/components/TitleText';
+import { useState, useEffect } from 'react';
+import { UserDataType } from '@/types/dataTypes';
+import RankListLine from '@/components/RankListLine';
+
+import userDataJSON from '@/test/userData.json';
+import rankDataJSON from '@/test/rankData.json';
+
+import {
+  getLocalUserData,
+  setLocalUserData,
+} from '@/stores/localStorageFunctions';
 
 export default function Rank() {
-  return <Text>Rank</Text>;
+  // 유저 정보
+  const [userData, setUserData] = useState<UserDataType | undefined>();
+  // 랭킹 리스트
+  const [rankList, setRankList] = useState<UserDataType[]>([]);
+
+  // 서버에 전송 -> 새로운 데이터 응답 (rank만 변화함)
+  const postUserData = async () => {
+    // 로컬 유저 데이터 가져옴
+
+    const localUserData = getLocalUserData();
+    console.log(localUserData);
+    if (!localUserData) return;
+
+    try {
+      // 서버에 전송
+      // const newUserData = await axios.post(localUser);
+
+      // 현재 로컬 유저 데이터 사용
+      const newUserData = localUserData;
+      if (!newUserData) return;
+
+      console.log(newUserData);
+
+      setUserData(newUserData);
+
+      if (newUserData !== localUserData) {
+        // 새로운 유저 데이터 로컬에 저장
+        setLocalUserData(newUserData);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // 서버에서 랭킹 정보를 가져옴
+  const getRankList = async () => {
+    try {
+      // 서버에서 가져오기
+      // const rank = await axios.get();
+
+      // 더미 데이터 사용
+      const rank: UserDataType[] = rankDataJSON;
+      console.log(rank);
+      if (rank) setRankList(rank);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    postUserData();
+  }, []);
+
+  useEffect(() => {
+    if (userData) getRankList();
+  }, [userData]);
+
+  return (
+    <>
+      <HeaderButton>랭킹</HeaderButton>
+      <View className="h-full bg-green-700">
+        {userData ? (
+          <ScrollView className="h-full bg-green-600">
+            {/* 유저 랭킹 표시 */}
+            <View className="h-60 w-full bg-green-700 items-center">
+              <TitleText size={30}>{userData.nickname}</TitleText>
+              <TitleText size={60}>랭킹 {userData.rank}등</TitleText>
+              <TitleText size={30}>최고점수 : {userData.topScore}점</TitleText>
+            </View>
+            {/* 랭킹 상위 100명 표시 */}
+            <View>
+              <RankListLine content={{ a: '랭킹', b: '사용자', c: '점수' }} />
+              {rankList.length > 0 &&
+                rankList.map((user, key) => (
+                  <RankListLine
+                    key={key}
+                    content={{
+                      a: user.rank.toString(),
+                      b: user.nickname,
+                      c: `${user.topScore}점`,
+                    }}
+                  />
+                ))}
+            </View>
+          </ScrollView>
+        ) : (
+          <View className="items-center">
+            <TitleText size={60}>사용자 정보를 불러오지 못했습니다.</TitleText>
+            <TitleText size={30}>다시 시도해주세요.</TitleText>
+          </View>
+        )}
+      </View>
+    </>
+  );
 }
