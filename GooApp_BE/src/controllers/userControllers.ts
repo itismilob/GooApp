a8941eab6f4e953cec83072c99e35a70c4ede236
itismilob <itismilob@gmail.com>
2025-06-19 19:49:54 +0900

    feat: 유저 컨트롤러 생성
    defaultTypes.ts : 컨트롤러 타입 선언

---
+++ GooApp_BE/src/controllers/userControllers.ts
@@ -0,0 +1,9 @@
+import type { Controller } from '@/types/defaultTypes';
+
+export const createUser: Controller = (req, res, next) => {};
+
+export const getUser: Controller = (req, res, next) => {};
+
+export const getTop100: Controller = (req, res, next) => {};
+
+export const updateUserScore: Controller = (req, res, next) => {};
