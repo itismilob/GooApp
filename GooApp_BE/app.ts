4bebd3ef2c215c1f8a09305d1260f4bb4dfb1f5c
itismilob <itismilob@gmail.com>
2025-06-20 18:33:18 +0900

    feat: 배포 버전에 helmet 추가(기본값)

--- GooApp_BE/app.ts
+++ GooApp_BE/app.ts
@@ -3,12 +3,14 @@ import { testRouter } from '@/routes/testRoutes';
 import { userRouter } from '@/routes/userRoutes';
 import errorMiddleware from '@/middlewares/errorMiddleware';
 import morgan from 'morgan';
+import helmet from 'helmet';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 if (process.env.ENV === 'production') {
   app.use(morgan('combined'));
+  app.use(helmet());
 } else {
   app.use(morgan('dev'));
 }
