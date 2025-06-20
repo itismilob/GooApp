e4ebaef78c35050a0e45d4970c08b7667f9d3a1e
itismilob <itismilob@gmail.com>
2025-06-20 17:52:11 +0900

    feat: morgan 로깅 추가

--- GooApp_BE/app.ts
+++ GooApp_BE/app.ts
@@ -2,9 +2,16 @@ import express from 'express';
 import { testRouter } from '@/routes/testRoutes';
 import { userRouter } from '@/routes/userRoutes';
 import errorMiddleware from '@/middlewares/errorMiddleware';
+import morgan from 'morgan';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+if (process.env.ENV === 'production') {
+  app.use(morgan('combined'));
+} else {
+  app.use(morgan('dev'));
+}
 
 app.use('/test', testRouter);
 app.use('/users', userRouter);
