8e5cd817f59aac347fa24429810a612ada3b7fc7
itismilob <itismilob@gmail.com>
2025-06-25 20:58:31 +0900

    feat: MongoDB 연결

--- GooApp_BE/app.ts
+++ GooApp_BE/app.ts
@@ -1,12 +1,28 @@
 import express from 'express';
-import { testRouter } from '@/routes/testRoutes';
 import { userRouter } from '@/routes/userRoutes';
 import errorMiddleware from '@/middlewares/errorMiddleware';
 import morgan from 'morgan';
 import helmet from 'helmet';
+import mongoose from 'mongoose';
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const { PORT, MONGODB_URI } = process.env;
+
+app.use(express.json());
+
+async function connectDB() {
+  try {
+    if (!MONGODB_URI) return;
+    await mongoose.connect(MONGODB_URI);
+    console.log('MongoDB 연결 성공');
+  } catch (err) {
+    console.error('MongoDB 연결 실패:', err);
+  }
+}
+connectDB();
 
 if (process.env.ENV === 'production') {
   app.use(morgan('combined'));
@@ -15,7 +31,6 @@ if (process.env.ENV === 'production') {
   app.use(morgan('dev'));
 }
 
-app.use('/test', testRouter);
 app.use('/users', userRouter);
 
 app.get('/', (req, res) => {
